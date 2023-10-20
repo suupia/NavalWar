df0ac0b3f2fab2441518f6a1c95405020ef8bad3
suupia <morita777k@icloud.com>
2023-10-21 00:04:04 +0900

    remove redundant logs

--- GameObjects/SortieButton.cpp
+++ GameObjects/SortieButton.cpp
@@ -8,11 +8,6 @@ void SortieButton::UpdateLogic()
 	if(rect_.leftClicked())
 	{
 		Print << U"Button Logic index{}"_fmt(index_);
-		//Ship(gm_, Vec2{0, 3.0/5.0 * Scene::Height()});
-		// ships_.push_back(std::make_unique<Ship>(gm_, Vec2{0, 2.0/5.0 * Scene::Height()}));  // 単体だとバグる。Clickにもあるとなぜが動く
-		// std::make_unique<Ship>(gm_, Vec2{0, 2.0/5.0 * Scene::Height()}); // バグる
-		// Ship(gm_, Vec2{0, 2.0/5.0 * Scene::Height()}); // バグる
-		// 生存期間が問題っぽい。たぶん、デストラクタが呼ばれて何かが消えているのだと思う。
 	}
  }
 void SortieButton::UpdateRender()
@@ -24,12 +19,9 @@ void SortieButton::UpdateRender()
 }
 void SortieButton::Click()
 {
-	// Print << U"Button Logic index{}"_fmt(index_);
 	if(rect_.leftClicked())
 	{
 		Print << U"Clicked index{}"_fmt(index_) ;
-		// ships_.push_back(std::make_unique<Ship>(gm_, Vec2{0, 3.0/5.0 * Scene::Height()}));
 		shipContainer_.AddShip(std::make_unique<Ship>(gm_, Vec2{0, 3.0/5.0 * Scene::Height()}));
-		//std::make_unique<Ship>(gm_, Vec2{0, 3.0/5.0 * Scene::Height()});
 	}
 }
