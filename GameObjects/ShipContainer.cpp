888f931e1acc97697f0327725b15086019f6e49e
suupia <morita777k@icloud.com>
2023-10-20 23:39:35 +0900

    Add ShipContainer

---
+++ GameObjects/ShipContainer.cpp
@@ -0,0 +1,17 @@
+﻿#include "../stdafx.h"
+#include "ShipContainer.h"
+
+void ShipContainer::UpdateLogic()
+{
+	for(const auto ship : ships_)
+	{
+		ship->UpdateLogic();
+	}
+}
+void ShipContainer::UpdateRender()
+{
+	for(const auto ship : ships_)
+	{
+		ship->UpdateRender();
+	}
+}
---
+++ GameObjects/ShipContainer.h
@@ -0,0 +1,34 @@
+﻿#include "Ship.h"
+
+class ShipContainer
+{
+private:
+	GameManager& gm_;
+	RectF rect_;
+	const Font& font_;
+	int index_;
+	int logicId_;
+	int renderId_;
+	std::vector<std::unique_ptr<Ship>> ships_;
+
+public:
+	ShipContainer(GameManager& gm, RectF rect, const Font& font, int index)
+		: gm_(gm), rect_(rect), index_(index), font_(font)
+	{
+		// Register functions and save their IDs
+		logicId_ = gm_.registerLogic([this]() { this->UpdateLogic(); });
+		renderId_ = gm_.registerRender([this]() { this->UpdateRender(); });
+	}
+
+	~ShipContainer()
+	{
+		// Unregister the callbacks in the destructor
+		Print << U"Destruct SortieButton";
+		gm_.unregisterLogic(logicId_);
+		gm_.unregisterRender(renderId_);
+
+}
+
+	void UpdateLogic();
+	void UpdateRender();
+};
